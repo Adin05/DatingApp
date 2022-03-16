2719f6ac6bb2ea15d1547214345c91abdc3851ad
adi nurr <adinurr@gmail.com>
2022-03-16 14:47:32 +0700

    section 7

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -9,8 +9,9 @@ namespace API.DTOs
     public class RegisterDto
     {
         [Required]
-        public string UserName { get; set; }    
+        public string UserName { get; set; }
         [Required]
+        [StringLength(8, MinimumLength = 4)]
         public string Password { get; set; }
     }
 }
